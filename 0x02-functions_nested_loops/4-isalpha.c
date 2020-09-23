ee0c350b1de67775bfcdcc14c0d2193fdc0197ad
josevelez08 <jose-0919@hotmail.com>
2020-09-23 00:16:04 +0000

    Task 4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ * _isalpha - writes the character c to stdout
+ * @c: parameters
+ * Return: On success 1.
+ */
+int _isalpha(int c)
+{
+	if (c > 64 || c < 91)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
