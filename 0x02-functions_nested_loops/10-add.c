4832916483d2680834c08f7386440d4e406421b6
josevelez08 <jose-0919@hotmail.com>
2020-09-24 02:54:26 +0000

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * add - check the code for Holberton School students.
+ * @i: first number
+ * @j: second number
+ * Return: Always 0.
+ */
+int add(int i, int j)
+{
+	int k;
+
+	k = i + j;
+
+	return (k);
+}
+
