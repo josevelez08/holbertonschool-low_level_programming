c1df9c7e60d7178453fe7910218cb1dd6ef3395a
josevelez08 <jose-0919@hotmail.com>
2020-09-24 21:48:40 +0000

    Multiplication

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * mul - check the code for Holberton School students.
+ * @a: number
+ * @b: number
+ * Return: Always 0.
+ */
+
+int mul(int a, int b)
+{
+	int k;
+
+	k = a * b;
+	return (k);
+}
