243adc0fd8c6d702657f24e0d4d3acbe440ae7bd
josevelez08 <jose-0919@hotmail.com>
2020-09-24 02:04:17 +0000

    Reloj

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+
+/**
+ * jack_bauer - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+void jack_bauer(void)
+{
+	int horas1, horas2, minutos1, minutos2 = 0;
+
+	for (horas1 = 0; horas1 < 3; horas1++)
+	{
+		for (horas2 = 0; horas2 < 4; horas2++)
+		{
+			for (minutos1 = 0; minutos1 < 6; minutos1++)
+			{
+				for (minutos2 = 0; minutos2 < 10; minutos2++)
+				{
+				_putchar(horas1 + 48);
+				_putchar(horas2 + 48);
+				_putchar(58);
+				_putchar(minutos1 + 48);
+				_putchar(minutos2 + 48);
+				_putchar(10);
+				}
+			}
+		}
+	}
+}
