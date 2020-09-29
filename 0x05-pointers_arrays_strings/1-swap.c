029f23fb41d93a1192752dddead038b02aae0555
josevelez08 <jose-0919@hotmail.com>
2020-09-29 20:08:04 +0000

    assingning two values to the pointers

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+ * swap_int - check the code for Holberton School students.
+ * @a: first pointer
+ * @b: second pointer
+ * Return: Always 0.
+ */
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
