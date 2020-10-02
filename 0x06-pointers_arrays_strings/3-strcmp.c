f1db96b831b0759d3e5c660fe541cd79ffa5b231
josevelez08 <jose-0919@hotmail.com>
2020-10-02 01:53:11 +0000

    Task 13

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -7,24 +7,29 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-  int i;
-  int j;
-  int k;
+	int i;
+	int j;
+	int k;
 
-  for ( i = 0; s1[i] != '\0'; i++);
-  for ( j = 0; s2[j] != '\0'; j++);
-  
-  if ( i > j)
-  {
-    k = 15;
-  }
-  else if ( j > i )
-  {
-    k = -15;
-  }
-  else
-  {
-    k = 0;
-  }
-  return(k);
-} 
+	for (i = 0; s1[i] != '\0';)
+	{
+		i++;
+	}
+	for (j = 0; s2[j] != '\0';)
+	{
+		j++;
+	}
+	if (i > j)
+	{
+		k = 15;
+	}
+	else if (j > i)
+	{
+		k = -15;
+	}
+	else
+	{
+		k = 0;
+	}
+	return (k);
+}
