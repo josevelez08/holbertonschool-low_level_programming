0768c5431c8f24450f9c5026bb17fa7787983886
josevelez08 <jose-0919@hotmail.com>
2020-10-05 23:12:40 +0000

    Task 3

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+/**
+ * _strspn - check the code for Holberton School students.
+ * @s: string
+ * @accept: string
+ * Return: Always 0.
+*/
+unsigned int _strspn(char *s, char *accept)
+{
+	int i = 0;
+	int j = 0;
+	int k = 0;
+
+	for (i = 0; s[i] != ','; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (accept[j] == s[i])
+			{
+				k++;
+			}
+		}
+	}
+	return (k);
+}
