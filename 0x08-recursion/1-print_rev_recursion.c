5bd1a22c27336c0c162baf0eb4650e0174a59f23
josevelez08 <jose-0919@hotmail.com>
2020-10-09 02:18:15 +0000

    Task 1: print reverse one string

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+ * _print_rev_recursion - check the code for Holberton School students.
+ * @s: string
+ * Return: Always 0.
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		s++;
+		_print_rev_recursion(s);
+		s--;
+		_putchar(*s);
+	}
+}
