903403bf0aeefd82f252e1d4b6ab9fb2007ed2d4
josevelez08 <jose-0919@hotmail.com>
2020-10-10 07:15:34 +0000

    Task 2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,30 +1,21 @@
-#include "holberton.h"
-#define NULL ((void *)0)
+#include <stdio.h>
+
 /**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
+ * _strchr - check the code for Holberton School students.
+ * @s: string
+ * @c: char
+ * Return: Always 0
  */
+
 char *_strchr(char *s, char c)
 {
-    unsigned int i;
-    int j = 0;
-    char a[20];
-    char *t = a;
-    for ( i = 0; s[i] != '\0';i++)
-    {
-        if ( s[i] == c )
-        {
-          while ( s[i] != '\0')
-          {
-            a[j] = s[i];
-            j++;
-            i++;
-          }
-        }
-    }
-    i = 0;
-    for ( i = 0; t[i] != '\0'; i++);
-    
-    return (t);
+	while (*s != '\0')
+	{
+		s++;
+		if (*s == c)
+		{
+			return (s);
+		}
+	}
+	return (0);
 }
