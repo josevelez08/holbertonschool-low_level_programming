b312ee4f1fd23f0ff80eb02b1b13d7931a80269b
josevelez08 <jose-0919@hotmail.com>
2020-10-10 07:31:54 +0000

    Task 4 try

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -5,22 +5,16 @@
  * @accept: string
  * Return: Always 0.
 */
+
 char *_strpbrk(char *s, char *accept)
 {
-	int i = 0;
-	int j = 0;
-
-	for (i = 0; s[i] != '\0'; i++)
+	while (*s != '\0')
 	{
-		for (j = 0; accept[j] != '\0'; j++)
+		s++;
+		if (*s == *accept)
 		{
-			if (s[i] == accept[j])
-			{
-				s++;
-				return (s);
-			}
+			return (s);
 		}
-			s++;
 	}
 	return (0);
 }
