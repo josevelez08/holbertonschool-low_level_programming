8d76de810c91dbedac9581c472ac3ba456654ad3
josevelez08 <jose-0919@hotmail.com>
2020-10-12 03:57:59 +0000

    Task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * main - proyecto argv, argc
+ * @argc: The number of command line arguments
+ * @argv: An array containing the program command line arguments
+ * Return: 0 always
+ *
+ */
+int main(int argc, char *argv[])
+{
+	argv++;
+	printf("%d\n", argc);
+
+	return (0);
+}
