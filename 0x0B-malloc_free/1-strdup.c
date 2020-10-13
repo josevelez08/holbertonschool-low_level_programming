f47c716e5c0471979903df2b91045914fe58907d
josevelez08 <jose-0919@hotmail.com>
2020-10-13 22:00:52 +0000

    Task 1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+ * _strdup - check the code for Holberton School students.
+ * @str: one string
+ * Return: Always 0.
+ */
+char *_strdup(char *str)
+{
+	int i;
+	char *s;
+
+	for (i = 0; str[i] != '\0';)
+	{
+		i++;
+	}
+	s = malloc(sizeof(char) * (i + 1));
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		s[i] = str[i];
+	}
+	s[i] = '\0';
+	return (s);
+}
--- 0x0B-malloc_free/holberton.h
+++ 0x0B-malloc_free/holberton.h
@@ -2,4 +2,5 @@
 #define HOLBERTON_H
 char *str_concat(char *s1, char *s2);
 char *create_array(unsigned int size, char c);
+char *_strdup(char *str);
 #endif
