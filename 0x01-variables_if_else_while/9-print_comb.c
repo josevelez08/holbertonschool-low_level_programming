99e4f1411d23147d2659927af661b860d61debe6
josevelez08 <jose-0919@hotmail.com>
2020-10-16 08:12:02 +0000

    Task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - code for the Holberton School
+ * Return: 0 always
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		putchar(i);
+		if (i == 57)
+		{
+			break;
+		}
+		putchar(',');
+		putchar(' ');
+	}
+		putchar(10);
+	return (0);
+}
