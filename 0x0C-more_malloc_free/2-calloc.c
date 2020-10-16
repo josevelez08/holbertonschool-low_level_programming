5e6b3aa97856dc4b37f84a1acb6b257fe2a0155d
josevelez08 <jose-0919@hotmail.com>
2020-10-16 02:35:40 +0000

    Task 2

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * _calloc - prints buffer in hexa
+ * @nmemb: the address of memory to print
+ * @size: the size of the memory to print
+ *
+ * Return: Nothing.
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *c;
+
+	if (nmemb == 0 || size == 0)
+	{
+		return (NULL);
+	}
+	c = malloc(nmemb * size);
+
+	if (c == NULL)
+	{
+		return (NULL);
+	}
+	return (c);
+}
