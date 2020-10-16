e217f3fb9e99c2525a99c19d34919a205d353dcb
josevelez08 <jose-0919@hotmail.com>
2020-10-16 07:37:16 +0000

    Task 2 print alphabete in lowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - code for the Holberton School
+ * Return: 0 always
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 97; i <= 122; i++)
+	{
+		putchar(i);
+	}
+	putchar(10);
+		return (0);
+}
