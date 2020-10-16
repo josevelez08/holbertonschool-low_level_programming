253baa9d24f35eb04899e41ba3d08f3f8249b775
josevelez08 <jose-0919@hotmail.com>
2020-10-16 07:48:57 +0000

    Task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - code for the Holberton School
+ * Return: 0 always
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 97; i <= 122; i++)
+	{
+		if (i == 113 || i == 101)
+		{
+			i++;
+		}
+		putchar(i);
+	}
+	putchar(10);
+	return (0);
+}
