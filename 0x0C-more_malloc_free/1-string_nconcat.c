5a3e91be9484f7f9300607fb0e72571bb389d078
josevelez08 <jose-0919@hotmail.com>
2020-10-16 01:32:18 +0000

    Task 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -17,10 +17,12 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (s1 == NULL)
 	{
 		s1 = "";
+		return (s1);
 	}
 	if (s2 == NULL)
 	{
 		s2 = "";
+		return (s2);
 	}
 	for (i = 0; s1[i] != '\0';)
 	{i++; }
