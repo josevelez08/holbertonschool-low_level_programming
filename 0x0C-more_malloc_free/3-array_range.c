d9dfebeeb83b9bc25522c08795e0c3c639dedff3
josevelez08 <jose-0919@hotmail.com>
2020-10-16 03:54:44 +0000

    Task 3

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -12,19 +12,20 @@
 
 int *array_range(int min, int max)
 {
-  int *u;
-  int p;
-  p  = 0;
-  u = malloc(sizeof(int) * (max - min + 1));
-  if (u == NULL)
-  {
+	int *u;
+	int p;
+	p  = 0;
+
+	u = malloc(sizeof(int) * (max - min + 1));
+	if (u == NULL)
+	{
 		free(u);
 		return (NULL);
-  }
-  for (p = 0; min <= max; p++)
-  {
-    u[p] = min;
-    min++;
-  }
-  return (u);
+	}
+	for (p = 0; min <= max; p++)
+	{
+		u[p] = min;
+		min++;
+	}
+	return (u);
 }
