3dd9cef5a289f104d4535cf0274f50c49557f688
josevelez08 <jose-0919@hotmail.com>
2020-10-16 07:52:16 +0000

    Task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - code for the Holberton School
+ * Return: 0 always
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+	printf("\n");
+	return (0);
+}
