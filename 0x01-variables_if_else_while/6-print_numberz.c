c143e544010bcaf133b8fc4ddc6fbb821c3b332a
josevelez08 <jose-0919@hotmail.com>
2020-10-16 07:56:17 +0000

    Task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - code for the Holberton School
+ * Return: 0 always
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		putchar(i);
+	}
+	putchar(10);
+	return (0);
+}
