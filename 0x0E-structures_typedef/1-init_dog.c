0006660e4283bf36b47703442b2ff822c659821c
josevelez08 <jose-0919@hotmail.com>
2020-10-19 22:58:43 +0000

    Task 1 initializating variables

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,15 @@
+#include "dog.h"
+/**
+ * init_dog - Short description
+ * @d: pointer of dog type
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
