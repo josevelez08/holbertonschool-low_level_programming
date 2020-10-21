71394cf1cd03c4200c6c1667a708bd5b1e51c30f
josevelez08 <jose-0919@hotmail.com>
2020-10-21 21:33:44 +0000

    Task 1

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -11,11 +11,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int i;
 
-	if (!action)
-	{
-	return;
-	}
-	else
+	if (action != '\0')
 	{
 		for (i = 0; i < size; i++)
 		{
