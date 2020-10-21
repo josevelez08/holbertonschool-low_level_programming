462ad4d4d7e88e4a1fc2653c2b2583b65857843a
josevelez08 <jose-0919@hotmail.com>
2020-10-21 22:14:21 +0000

    Task 2 search a integer

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,22 @@
+#include <stddef.h>
+#include "function_pointers.h"
+#include <stdio.h>
+
+/**
+ * is_98 - check if a number is equal to 98
+ * @array: the integer to check
+ * @size: tamaño del array
+ * @cmp: funtion to pointer
+ * Return: 0 if false, something else otherwise.
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+	int j = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		j = cmp(array[i]) + j;
+	}
+	return (j);
+}
