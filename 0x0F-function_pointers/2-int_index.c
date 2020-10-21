b9ebbb5226d72659ced44a64ac29afeb6ceb31e1
josevelez08 <jose-0919@hotmail.com>
2020-10-21 22:17:40 +0000

    Task 2

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -14,9 +14,18 @@ int int_index(int *array, int size, int (*cmp)(int))
 	int i;
 	int j = 0;
 
+	if (size <= 0)
+	{
+		return (-1);
+	}
+
 	for (i = 0; i < size; i++)
 	{
 		j = cmp(array[i]) + j;
+	}
+	if (j == 0)
+	{
+		return (-1);
 	}
 	return (j);
 }
