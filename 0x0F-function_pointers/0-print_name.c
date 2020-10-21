18413a4d2183e2d716895b256bc9c6b6d463ef49
josevelez08 <jose-0919@hotmail.com>
2020-10-21 21:18:08 +0000

    Task 0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,10 @@
+#include "function_pointers.h"
+/**
+ * print_name - print name
+ * @name: pointer to char
+ * @f: funtion to pinter
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
---
+++ 0x0F-function_pointers/README.md
@@ -0,0 +1,1 @@
+This the folder for the projec the function to pointer
