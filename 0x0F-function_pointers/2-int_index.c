45e731b11711cba97857e25a29a644f1e676ff7e
josevelez08 <jose-0919@hotmail.com>
2020-10-22 20:33:47 +0000

    Task 2

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -18,8 +18,12 @@ int int_index(int *array, int size, int (*cmp)(int))
 	{
 		return (-1);
 	}
+	if (array == NULL)
+	{
+		return (-1);
+	}
 
-	if (cmp != '\0' && size > 0 && array != '\0')
+	if (cmp != '\0' && size && array != '\0')
 	{
 	for (i = 0; i < size; i++)
 	{
@@ -33,6 +37,10 @@ int int_index(int *array, int size, int (*cmp)(int))
 		{
 			return (-1);
 		}
+	}
+	else
+	{
+	return (-1);
 	}
 	return (0);
 }
