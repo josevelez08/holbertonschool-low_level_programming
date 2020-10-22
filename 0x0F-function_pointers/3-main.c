4bf079f3ffacbbdeccb9b67d7b4e05cb4f404ee9
josevelez08 <jose-0919@hotmail.com>
2020-10-22 22:29:49 +0000

    Task 3

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -5,32 +5,34 @@
  * @argv: array
  * Return: 0 always
  */
-int main(int argc, char *argv[])
+	int main(int argc, char *argv[])
 {
-	int num1;
-	int num2;
-	int result;
+int num1;
+int num2;
+int result;
 
+num1 = atoi(argv[1]);
+num2 = atoi(argv[3]);
 
-	if (argc == 4)
+	if (argc != 4)
 	{
-		num1 = atoi(argv[1]);
-		num2 = atoi(argv[3]);
-		result = get_op_func(argv[2])(num1, num2);
-		if (result == 0)
-		{
-			printf("%d\n", result);
-			return (0);
-		}
-		if (result == '\0')
-		{
-		printf("Error\n");
-		exit(99);
-		}
-		printf("%d\n", result);
-	return (0);
+		printf("Error %d\n", argc);
+		exit(98);
 	}
 
+
+	result = get_op_func(argv[2])(num1, num2);
+	if (result == 0)
+	{
+		printf("%d\n", result);
+		return (0);
+	}
+	if (result == '\0')
+	{
 		printf("Error\n");
-		exit(98);
+		exit(99);
+	}
+	printf("%d\n", result);
+
+	return (0);
 }
