7c2a3297dcaab2300d31cd44da128f189fceba66
josevelez08 <jose-0919@hotmail.com>
2020-10-23 23:30:36 +0000

    andvanced task

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+/**
+ * main - the advanced task
+ *
+ */
+int main(void)
+{
+int i;
+int j;
+
+for (i = 0; i < 10; i++)
+{
+	for (j = 0; j < 10; j++)
+	{
+		if (i > 0 && j == 0)
+		{ j++; }
+		if (i > 1 && j == 1)
+		{ j++; }
+		if (i > 2 && j ==  2)
+		{ j++; }
+		if (i > 3 && j == 3)
+		{ j++; }
+		if (i > 4 && j == 4)
+		{ j++; }
+		if (i > 5 && j == 5)
+		{ j++; }
+		if (i > 6 && j == 6)
+		{ j++; }
+		if (i > 7 && j == 7)
+		{ j++; }
+		if (i > 8 && j == 8)
+		{ j++; }
+		if (j == i)
+		{ j++; }
+		putchar(i + 48);
+		putchar(j + 48);
+		if (i == 8 && j == 9)
+		{
+		return (0);
+		}
+		putchar(',');
+		putchar(' ');
+	}
+	}
+	putchar(10);
+	return (0);
+}
