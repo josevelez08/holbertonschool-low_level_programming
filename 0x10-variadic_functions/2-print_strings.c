27a98323055621aff8ab67ac293c27dbdbbaf7cc
josevelez08 <jose-0919@hotmail.com>
2020-10-25 18:25:43 +0000

    Task 2 variadic funtions

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,26 @@
+#include "variadic_functions.h"
+/**
+ * print_strings - check the code for Holberton School students.
+ * @separator: the separator
+ * @n: size of the sentence
+ * Return: Always 0.
+*/
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i = 0;
+	char *j;
+va_list valist;
+
+va_start(valist, n);
+
+for (i = 0; i < n; i++)
+{
+	j = va_arg(valist, char *);
+	printf("%s", j);
+	if (i == 0)
+	{
+		printf("%c ", *separator);
+	}
+}
+printf("\n");
+}
