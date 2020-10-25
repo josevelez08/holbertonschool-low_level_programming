884267a729e3ca53e89d9220a895f134a177dfb5
josevelez08 <jose-0919@hotmail.com>
2020-10-25 18:41:46 +0000

    Header file

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,5 +1,8 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
 #include <stdarg.h>
 void print_numbers(const char *separator, const unsigned int n, ...);
 int sum_them_all(const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 #include <stdio.h>
+#endif
