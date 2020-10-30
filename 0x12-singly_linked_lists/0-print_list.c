09fc7f2a8bb2daa290d26113daa1b92654f047b4
josevelez08 <jose-0919@hotmail.com>
2020-10-30 07:31:19 +0000

    The firs commit for the solution of the project

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ * print_list - print nodes of a singled linked list
+ * @h: pointer of type list_T
+ * Return: number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	if (h->str == NULL)
+	{
+	printf("[0] (nil)\n");
+	}
+	else
+	{
+		printf("[%d] ", h->len);
+printf("[%s]\n", h->str); }
+printf("[%d] ", h->len);
+printf("[%s]\n", h->next->str);
+
+return (2);
+}
