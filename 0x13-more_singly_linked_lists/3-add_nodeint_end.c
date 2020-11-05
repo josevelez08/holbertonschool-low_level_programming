b1f85c74401fe7cc50e81e807ffc0e1247154b83
josevelez08 <jose-0919@hotmail.com>
2020-11-05 00:11:58 +0000

    Fix

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -11,6 +11,10 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	listint_t *iterator = NULL;
 
 	newElement = ((listint_t *)malloc(sizeof(listint_t)));
+	if (newElement == NULL)
+	{
+		return (NULL);
+	}
 
 	newElement->n = n;
 	newElement->next = NULL;
