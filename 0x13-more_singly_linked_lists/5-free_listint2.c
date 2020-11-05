1e99a8efb92cb89658c876833312d310913ae574
josevelez08 <jose-0919@hotmail.com>
2020-11-05 01:23:29 +0000

    Task 5  free

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * free_listint2 - free
+ * @head: double pointer
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *c;
+
+	while ((*head)->next != NULL)
+	{
+		c = *head;
+		*head = (*head)->next;
+		free(c);
+	}
+	free(*head);
+}
