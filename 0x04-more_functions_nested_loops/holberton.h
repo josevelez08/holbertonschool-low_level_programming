b4370dfcce9fe2f8165645c8ef0f8182e235eb0b
josevelez08 <jose-0919@hotmail.com>
2020-11-30 16:36:06 +0000

    header file

--- 0x04-more_functions_nested_loops/holberton.h
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -1,8 +1,6 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
-
-#include <stdio.h>
-
+int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
@@ -13,5 +11,5 @@ void print_line(int n);
 void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
-int _putchar(char c);
+void print_number(int n);
 #endif
