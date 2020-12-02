4989c43d22ffc2cde1b8102efbe79be1484379d0
josevelez08 <jose-0919@hotmail.com>
2020-12-02 17:58:30 +0000

    Task 2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -8,12 +8,29 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i = 0;
-	int j = 0;
+	int c;
 
-	for (i = 0; i != n;)
-	{
-		dest[i++] =  src[j++];
-	}
+	for (c = 0; c < n && src[c] != '\0'; c++)
+		dest[c] = src[c];
+	for ( ; c < n; c++)
+		dest[c] = '\0';
 	return (dest);
+}
+
+/**
+ * _strlen - calculate the length of the string
+ * @s: address of that variable
+ * Return: return the length of a string
+ */
+int _strlen(char *s)
+{
+	int length;
+
+	length = 0;
+	while (*s != '\0')
+	{
+		length++;
+		s++;
 	}
+	return (length);
+}
