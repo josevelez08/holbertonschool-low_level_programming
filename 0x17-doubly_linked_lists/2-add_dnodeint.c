66f66167a224cd30078a970277ec0a5b621a8619
josevelez08 <jose-0919@hotmail.com>
2020-12-14 06:48:57 +0000

    Task 2

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -10,6 +10,10 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *newNode;
 
+	if (head == NULL)
+		return (NULL);
+
+
 	newNode = malloc(sizeof(dlistint_t));
 	if (newNode == NULL)
 	{
