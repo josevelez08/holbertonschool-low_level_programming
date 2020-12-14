00c37a144802e09d682f87089f92e0ce26d9a84f
josevelez08 <jose-0919@hotmail.com>
2020-12-14 10:56:39 +0000

    Task 3

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -5,16 +5,15 @@
  * @n: data
  * Return: adrres
  */
-
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
 	dlistint_t *newNode;
-	dlistint_t *temp1;
+	dlistint_t *temp1, *temp2;
 
 	newNode = malloc(sizeof(dlistint_t));
 	if (newNode == NULL)
 	{
-	return (NULL);
+		return (NULL);
 	}
 	newNode->n = n;
 	newNode->next = NULL;
@@ -26,12 +25,19 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	else
 	{
 	temp1 = *head;
+	temp2 = *head;
 
 	while (temp1->next != NULL)
 	{
 		temp1 = temp1->next;
+		if (temp1->next == NULL)
+		{
+			temp1->prev = temp2;
+		}
+		temp2 = temp2->next;
 	}
 	temp1->next = newNode;
 	}
-	return (temp1);
+	return (*head);
 }
+
