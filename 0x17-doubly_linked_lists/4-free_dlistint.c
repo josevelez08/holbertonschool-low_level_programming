f124497346f468016b99c978553504cfed1bfe73
josevelez08 <jose-0919@hotmail.com>
2020-12-14 16:23:48 +0000

    Task 4

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+/**
+ * free_dlistint - free a linked list
+ * @head: copy of the head of lk list
+ *
+ * Return: void
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *temp1;
+
+	if (head == NULL)
+	{
+		return (NULL);
+	}
+
+	while (head != NULL)
+	{
+		temp1 = head;
+		head = head->next;
+		free(temp1);
+	}
+}
