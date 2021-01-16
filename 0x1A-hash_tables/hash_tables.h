a19df6a2e45b9968c3d202f4969c1c26ea074338
josevelez08 <jose-0919@hotmail.com>
2021-01-16 00:22:15 +0000

    Task 1 the algorithm djb2

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -29,6 +29,7 @@ typedef struct hash_table_s
 	hash_node_t **array;
 } hash_table_t;
 hash_table_t *hash_table_create(unsigned long int size);
+unsigned long int hash_djb2(const unsigned char *str);
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
